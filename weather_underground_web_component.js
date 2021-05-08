1982b5f55026895f8c86f8f305a9905063d7f155
Tim Uckun <timuckun@gmail.com>
2021-05-08 13:06:48 +1200

    WIP. Hworking on a better way to humanize and assign units

--- weather_underground_web_component.js
+++ weather_underground_web_component.js
@@ -9,14 +9,13 @@ import { T } from "./test.js";
 class WundergroundStation extends HTMLElement {
   constructor() {
     super();
-    this.station={};
-    
+    this.station = {};
 
     const station = this.getAttribute("station");
     if (!station) {
       throw "station attribute is required";
     }
-    this.station.id=station;
+    this.station.id = station;
 
     const api_key = this.getAttribute("api_key") || secret_api_key;
     if (!api_key) {
@@ -44,7 +43,7 @@ class WundergroundStation extends HTMLElement {
     if (!template) {
       throw "specified template " + template_id + " does not exist in the DOM ";
     }
-  
+
     this.template = template;
     this.attachShadow({ mode: "open" });
 
@@ -70,131 +69,161 @@ class WundergroundStation extends HTMLElement {
     this.refresh();
   } //connectedCallback
 
-   
   render_template(template) {
     var ex = /{{\s*(.+?)\s*}}/;
     while (template.match(ex)) {
       const match = template.match(ex);
-      const value = eval('this.' + match[1])
-      if (value){
-      template = template.replace(match[0], value);
-      }else{
-        template= template.replace(match[0], "'" + match[1] + "' is not defined")
+      const value = eval("this." + match[1]);
+      if (value) {
+        template = template.replace(match[0], value);
+      } else {
+        template = template.replace(
+          match[0],
+          "'" + match[1] + "' is not defined"
+        );
       }
     }
     return template;
   }
-  
-  async refresh() {
 
+  async refresh() {
     await this.fetchWeather();
     //T.test();
     let templateContent = this.templateContent;
     //console.log(this.template.innerHTML)
     //console.log(this.templateContent)
-   // console.log(this.templateContent.innerHTML);
+    // console.log(this.templateContent.innerHTML);
     ///let templateContent = this.template.innerHTML;
 
-  
-     //templateContent = Template.render(templateContent,this);
-    templateContent=this.render_template(templateContent);
+    //templateContent = Template.render(templateContent,this);
+    templateContent = this.render_template(templateContent);
 
     //this.shadowRoot.appendChild(templateContent.cloneNode(true));
     this.shadowRoot.innerHTML = templateContent;
     // now that I have the observation and the forecast I can go ahead and populate more of the dib
   }
 
- 
-   async fetchWeather() {
+  async fetchWeather() {
     const cache_key = this.station.id + "_observation";
-       // I am not sure how often the updates are.
+    // I am not sure how often the updates are.
     const cache_duration = Cache.ONE_MINUTE * 15;
-    const obs =  await this.memoize(cache_key, cache_duration, async () => {
-      return await Api.fetchPwsObservation(this.station.id, this.units.abbrev, this.api_key);
-     });
-     console.log(obs);
-      // map the metrics to the top level object to make more sense for the template rendering
-      this.setObservation(obs);   
+    const obs = await this.memoize(cache_key, cache_duration, async () => {
+      return await Api.fetchPwsObservation(
+        this.station.id,
+        this.units.abbrev,
+        this.api_key
+      );
+    });
+    console.log(obs);
+    // map the metrics to the top level object to make more sense for the template rendering
+    this.setObservation(obs);
     //const cache_duration = Cache.NEVER;
-   //const observation = await Api.fetchPwsObservation(this.station.id, this.units.abbrev, this.api_key);
-   //console.log(observation);
-//    {
-//     "stationID": "ITAURA99",
-//     "obsTimeUtc": "2021-04-01T10:30:43Z",
-//     "obsTimeLocal": "2021-04-01 23:30:43",
-//     "neighborhood": "Whakamarama",
-//     "softwareType": null,
-//     "country": "NZ",
-//     "solarRadiation": null,
-//     "lon": 175.981,
-//     "realtimeFrequency": null,
-//     "epoch": 1617273043,
-//     "lat": -37.737,
-//     "uv": null,
-//     "winddir": 248,
-//     "humidity": 85,
-//     "qcStatus": -1,
-//     "metric": {
-//         "temp": 13,
-//         "heatIndex": 13,
-//         "dewpt": 10,
-//         "windChill": 13,
-//         "windSpeed": 3,
-//         "windGust": 3,
-//         "pressure": 1022.01,
-//         "precipRate": 0,
-//         "precipTotal": 0.25,
-//         "elev": 311
-//     }
-// }
-   //2021-04-01 23:30:07
-   //const obs = Api.fetchPwsObservation(this.station.id, this.units.abbrev, this.api_key)
-
-   
-  
+    //const observation = await Api.fetchPwsObservation(this.station.id, this.units.abbrev, this.api_key);
+    //console.log(observation);
+    //    {
+    //     "stationID": "ITAURA99",
+    //     "obsTimeUtc": "2021-04-01T10:30:43Z",
+    //     "obsTimeLocal": "2021-04-01 23:30:43",
+    //     "neighborhood": "Whakamarama",
+    //     "softwareType": null,
+    //     "country": "NZ",
+    //     "solarRadiation": null,
+    //     "lon": 175.981,
+    //     "realtimeFrequency": null,
+    //     "epoch": 1617273043,
+    //     "lat": -37.737,
+    //     "uv": null,
+    //     "winddir": 248,
+    //     "humidity": 85,
+    //     "qcStatus": -1,
+    //     "metric": {
+    //         "temp": 13,
+    //         "heatIndex": 13,
+    //         "dewpt": 10,
+    //         "windChill": 13,
+    //         "windSpeed": 3,
+    //         "windGust": 3,
+    //         "pressure": 1022.01,
+    //         "precipRate": 0,
+    //         "precipTotal": 0.25,
+    //         "elev": 311
+    //     }
+    // }
+    //2021-04-01 23:30:07
+    //const obs = Api.fetchPwsObservation(this.station.id, this.units.abbrev, this.api_key)
   }
-   setObservation(obs){
+  async getObservation(){
+
+  }
+  setObservation(observation) {
+    // This is going to be populated and returned.
+    const h ={};
+    // Some mappings.
+    const obs = {
+      country: "NZ",
+      epoch: "epoch",
+      humidity: "humidity",
+      lat: "lat",
+      lon: "lon",
+      neighborhood: "neighborhood",
+      obsTimeLocal: "localTime",
+      obsTimeUtc: "utcTime",
+      qcStatus: "status",
+      realtimeFrequency: "realtimeFrequency",
+      softwareType: "softwareType",
+      solarRadiation: "solarRadiation",
+      stationID: "stationID",
+      uv: "uv",
+      winddir: "windDirection",
+    };
+    // metrics have units.
+    const metrics = {
+      dewpt: ["dewPoint", 'temp'],
+      elev: ["elevation", 'alt'],
+      heatIndex: ["heatIndex",'temp'],
+      precipRate: ["precipRate",
+      precipTotal: "precipTotal",
+      pressure: "pressure",
+      temp: "temp",
+      windChill: "windChill",
+      windGust: "windGust",
+      windSpeed: "windSpeed",
+    };
 
     // This function extracts the data from the observation and  normalizes them
-    const h = {}
+    const h = {};
     // Fist extract the station information
-    this.station.country=obs.country;
-    this.station.elevation=obs.metric.elev;
-    this.station.lat=obs.lat;
+    this.station.country = obs.country;
+    this.station.elevation = obs.metric.elev;
+    this.station.lat = obs.lat;
     this.station.lon;
-    this.station.neighborhood=obs.neighborhood;
+    this.station.neighborhood = obs.neighborhood;
 
     // Not used...
-  
+
     // solarRadiation: null
-   // uv: null
+    // uv: null
     //some data is at the root object
 
-   
-    h.heatIndex=obs.heatIndex;
-    h.humidity=obs.humidity;
-    h.temp=obs.temp;
-    const time=new Date(obs.obsTimeLocal);
-    h.time=Utils.formatDateTime(time, this.locale);
+    h.heatIndex = obs.heatIndex;
+    h.humidity = `${obs.humidity} %`;
+    h.temp = obs.temp;
+    const time = new Date(obs.obsTimeLocal);
+    h.time = Utils.formatDateTime(time, this.locale);
     // obsTimeUtc: "2021-04-17T11:58:02Z"
-    h.precipRate=obs.precipRate;
-    h.precipTotal=obs.precipTotal;
-    h.pressure=obs.pressure;
-    h.solarRadiation=obs.solarRadiation;
-    h.uv=obs.uv;
-    h.windDirection=obs.windir; // wind direction is at the top level but speed is in metric
+    h.precipRate = obs.precipRate;
+    h.precipTotal = obs.precipTotal;
+    h.pressure = obs.pressure;
+    h.solarRadiation = obs.solarRadiation;
+    h.uv = obs.uv;
+    h.windDirection = obs.windir; // wind direction is at the top level but speed is in metric
     // qcStatus: 1
 
-    
     // temp: 8
- 
-    
- 
-
 
     // metric:
-    h.dewPoint=obs.metric.dewpt;
-    
+    h.dewPoint = obs.metric.dewpt;
+
     // heatIndex: 8
     // precipRate: 0
     // precipTotal: 0
@@ -204,7 +233,7 @@ class WundergroundStation extends HTMLElement {
     // windGust: 0
     // windSpeed: 0
     // __proto__: Object
-  
+
     // obsTimeLocal: "2021-04-17 23:58:02"
     // obsTimeUtc: "2021-04-17T11:58:02Z"
     // precipRate: 0
@@ -222,8 +251,8 @@ class WundergroundStation extends HTMLElement {
     // windSpeed: 0
     // winddir: 225
 
-    this.observation=h;
-   }
+    this.observation = h;
+  }
   async fetchWeather_old() {
     const current = await Api.fetchPwsObservation(
       this.station,
@@ -246,8 +275,6 @@ class WundergroundStation extends HTMLElement {
     // const neighborhood = current.neighborhood;
     // this.setStationLink(neighborhood);
   }
-
-
 
   setStationLink(desc) {
     const div = this.getElement(".wunderground_stationID");
@@ -279,13 +306,15 @@ class WundergroundStation extends HTMLElement {
     //cache_key = station + "_observation";
 
     let retval = Cache.get(cache_key);
-    
+
     // sometimes you get empty objects
-    if (retval  // 👈 null and undefined check 
-      && !( Object.keys(retval).length === 0 && retval.constructor === Object)) {
+    if (
+      retval && // 👈 null and undefined check
+      !(Object.keys(retval).length === 0 && retval.constructor === Object)
+    ) {
       return retval;
     }
-  
+
     retval = await func();
     //retval could be a promise.
     console.log(retval);
